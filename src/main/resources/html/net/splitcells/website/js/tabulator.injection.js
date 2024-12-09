a490bada89153ed8b4862c7adce0ec88613aba20
Mārtiņš Avots <martins.avots@splitcells.net>
2024-12-09 22:37:07 +0100

    Draft structured reasoning pop up #51
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/resources/html/net/splitcells/website/js/tabulator.injection.js
+++ src/main/resources/html/net/splitcells/website/js/tabulator.injection.js
@@ -1,3 +1,20 @@
+function rowPopuper(e, row, onRendered) {
+    // TODO Make the data selection configurable.
+    return row.getData().reasoning.replaceAll("\\n", "<br />");
+    }
+function defaultColumnFormatter (cell, formatterParams, onRendered){
+    if (cell.getValue() == undefined) {
+        return undefined;
+    }
+    return cell.getValue();
+    };
 $(".net-splitcells-website-visually-replaceable").each((index, table) => {
-    var visualization = new Tabulator(table, {});
-});
+    let config = {rowClickPopup:rowPopuper};
+    let columns = [];
+    config.columns = columns;
+    let ths = table.querySelector('tbody').querySelector('tr').querySelectorAll('th');
+    for (i = 0; i < ths.length; i++) {
+        columns.push({title: "" + ths[i].innerHTML, formatter:defaultColumnFormatter});
+    }
+    let visualization = new Tabulator(table, config);
+    });
