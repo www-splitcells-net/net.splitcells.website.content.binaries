b6ebe9647d7156585236ffb61c2944926bdf9e2b
Mārtiņš Avots <martins.avots@splitcells.net>
2024-12-09 23:04:31 +0100

    Config content column for pop ups #51
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

--- src/main/resources/html/net/splitcells/website/js/tabulator.injection.js
+++ src/main/resources/html/net/splitcells/website/js/tabulator.injection.js
@@ -1,7 +1,3 @@
-function rowPopuper(e, row, onRendered) {
-    // TODO Make the data selection configurable.
-    return row.getData().reasoning.replaceAll("\\n", "<br />");
-    }
 function defaultColumnFormatter (cell, formatterParams, onRendered){
     if (cell.getValue() == undefined) {
         return undefined;
@@ -9,12 +5,22 @@ function defaultColumnFormatter (cell, formatterParams, onRendered){
     return cell.getValue();
     };
 $(".net-splitcells-website-visually-replaceable").each((index, table) => {
-    let config = {rowClickPopup:rowPopuper};
+    let config = {};
     let columns = [];
     config.columns = columns;
     let ths = table.querySelector('tbody').querySelector('tr').querySelectorAll('th');
+    var popColumn = undefined;
     for (i = 0; i < ths.length; i++) {
+        if (hasClass(ths[i], 'net-splitcells-website-table-popup-via-column-content')) {
+            popColumn = ths[i].innerHTML;
+        }
         columns.push({title: "" + ths[i].innerHTML, formatter:defaultColumnFormatter});
     }
+    if (popColumn != undefined) {
+        config.rowClickPopup = function (e, row, onRendered) {
+            // TODO Make the data selection configurable.
+            return row.getData()[popColumn].replaceAll("\\n", "<br />");
+            }
+        }
     let visualization = new Tabulator(table, config);
     });
